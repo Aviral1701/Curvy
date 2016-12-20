124898bef14f81df2ae802d314ecbcdb5c20f239
Aviral Verma <aviral1701@gmail.com>
2016-12-20 12:12:31 +0530

    changed keys for multiplayer

--- common.js
+++ common.js
@@ -12,42 +12,15 @@ var bgColor = "#000000";
 var playerColors = [ "#ff4500","#22a4ff", "#22ff33", "#ff2fd3"];
 var playerNames = ["Orange", "Blue", "Green", "Pink"];
 
-var increasespeed=function(){
-        speed=speed+0.1  ;
-	resetCanvas() ;
-        
-} ;
-var decreasespeed=function(){
-	if(speed>0.4) 
-	{
-         speed=speed-0.1 ;
-         resetCanvas() ;
-        }
-};
-var incangspeed=function(){
-	
-	angularSpeed+=0.001 ;
-	resetCanvas() ;
-} ;
-var decangspeed=function(){
-	if(angularSpeed>0.007)
-	{
-          angularSpeed-=0.001 ;
-          resetCanvas() ; 
-	}
-} ;
-
-
 var keyCodes = [
 	37,
 	39,
-	83,
+        65,
 	68,
 	66,
-	78,
-	192,
-	49,
-	65,
+	77,
+	85,
+	79,
 	87,
 	70,
 	38,
@@ -58,13 +31,12 @@ var keyCodes = [
 var keyNames = [
 	'left',
 	'right',
-	's',
+	'a',
 	'd',
 	'b',
-	'n',
-	'`',
-	'1',
-	'a',
+	'm',
+	'u',
+	'o',
 	'w',
 	'f',
 	'down',
